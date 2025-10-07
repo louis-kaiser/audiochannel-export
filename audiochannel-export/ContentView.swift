f725622db140da980fffa9f5afeccdc9bcef66d3
louis-kaiser <louis.kaiser0203@icloud.com>
2025-10-07 17:08:56 +0200

    Initial Commit

---
+++ audiochannel-export/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  audiochannel-export
+//
+//  Created by Louis Kaiser on 07.10.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
